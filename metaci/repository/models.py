ee68c031d92dd699be6fb4d02188e49d7baaa51d
ADARSH <adarshsngh73@gmail.com>
2018-11-30 12:42:07 +0530

    Added public field to Repository model

--- metaci/repository/models.py
+++ metaci/repository/models.py
@@ -32,6 +32,7 @@ class Repository(models.Model):
     owner = models.CharField(max_length=255)
     github_id = models.IntegerField(null=True, blank=True)
     url = models.URLField(max_length=255)
+    public = models.BooleanField(default=True)
 
     release_tag_regex = models.CharField(max_length=255, blank=True, null=True)
 
